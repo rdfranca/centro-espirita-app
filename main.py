e1b73471a322c0490bcc4ee1f893aaf2c4986e72
rdfranca <rodrigodfranca@gmail.com>
2025-07-14 23:37:35 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 
 from flask import Flask, render_template, request, redirect, jsonify
 import psycopg2
+import os
 
 app = Flask(__name__)
 
 def conectar():
     return psycopg2.connect(
-        host="localhost",
-        database="seubanco",
-        user="seuusuario",
-        password="suasenha"
+        host=os.environ.get("DB_HOST"),
+        database=os.environ.get("DB_NAME"),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        port=os.environ.get("DB_PORT", 5432)
     )
 
 @app.route("/cadastrar")
